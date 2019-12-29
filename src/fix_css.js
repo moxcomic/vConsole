ce33bce4bd0e415d85dae31febd08dd71e4a24a2
moxcomic <moxcomic@gmail.com>
2019-12-29 16:01:00 +0800

    update

---
+++ src/fix_css.js
@@ -0,0 +1,17 @@
+document.querySelector('style').innerHTML = `#layaContainer {
+  color: black;
+  font-size: 50px;
+}
+#layaContainer textarea.pos_left
+{
+  position:relative;
+  left:-20000px
+}
+#layaContainer .pos_left{
+  position:relative;
+  left:-2000000px
+}
+.vc-mask {
+  overflow: hidden !important;
+}
+`
